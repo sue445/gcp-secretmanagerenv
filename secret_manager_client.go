d549425ef4130e67b7ea3176e38d16361b094a36
sue445 <sue445@sue445.net>
2020-09-21 22:42:00 +0900

    Add secretManagerClient

---
+++ secret_manager_client.go
@@ -0,0 +1,11 @@
+package secretmanagerenv
+
+import (
+	"context"
+	"github.com/googleapis/gax-go/v2"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+)
+
+type secretManagerClient interface {
+	AccessSecretVersion(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error)
+}
